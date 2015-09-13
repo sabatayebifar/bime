edc6ecae07619b8c1f5e604b7c88df0424151a7d
saba TayebiFar <saba.TayebiFar@gmail.com>
2015-09-13 11:29:43 +0330

    add organization

---
+++ src/model/Person.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: s-tayebifar
+ * Date: 9/13/15
+ * Time: 11:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+}
